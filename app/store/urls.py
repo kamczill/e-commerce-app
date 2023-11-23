8f0310be213c2f64ee52178a83d93c0f0fecc579
kamczill <kam134@interia.eu>
2023-11-23 17:46:32 +0100

    Update store urls, Add permission classes and crud views

--- app/store/urls.py
+++ app/store/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
-from .views import ProductListView
+from .views import (
+    CategoryListCreateView,
+    CategoryRetrieveUpdateDestroyView,
+    ProductListView,
+    ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView
+)
 
 urlpatterns = [
     # ... other url patterns ...
+    path('categories/', CategoryListCreateView.as_view(), name='category-list'),
+    path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-detail'),
     path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('products/create/', ProductCreateView.as_view(), name='product-create'),
+    path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='product-update'),
+    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product-delete'),
+
 ]
--- app/store/views.py
+++ app/store/views.py
@@ -1,12 +1,77 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
-from .serializers import ProductSerializer
-from .models import Product
+from .serializers import ProductSerializer, CategorySerializer
+from .models import Product, Category
 
-class ProductListView(ListAPIView):
+# custom permission class
+class IsMerchantUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_merchant
+
+class IsMerchantOrSuperuser(permissions.BasePermission):
+    """
+    Allows access only to merchant or superuser users for write operations.
+    """
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and (request.user.is_merchant or request.user.is_superuser)
+
+class IsSuperuser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_superuser
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsMerchantOrSuperuser]
+
+class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsSuperuser]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.AllowAny()]
+        return [permissions.IsAdminUser()]
+
+class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['name', 'category', 'description', 'price']
     ordering_fields = ['name', 'category', 'price']
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductCreateView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsMerchantUser]
+
+    def perform_create(self, serializer):
+        serializer.save(merchant=self.request.user)
+
+class ProductUpdateView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsMerchantUser]
+
+    def get_queryset(self):
+        """Ensure a merchant can only update their own products."""
+        user = self.request.user
+        return user.products.all()
+
+class ProductDeleteView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsMerchantUser]
+
+    def get_queryset(self):
+        """Ensure a merchant can only update their own products."""
+        user = self.request.user
+        return user.products.all()
