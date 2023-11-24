b9a2de93028d8f86a96eaf941171e7e70d9e81c2
kamczill <kam134@interia.eu>
2023-11-24 08:19:25 +0100

    fix: Fix import bug in tasks.py

--- app/store/tasks.py
+++ app/store/tasks.py
@@ -1,9 +1,9 @@
 from celery import shared_task
 from django.core.mail import send_mail
-from .models import Order
 
 @shared_task
 def send_payment_reminder_email(order_id):
+    from .models import Order
     order = Order.objects.get(id=order_id)
 
     send_mail(
